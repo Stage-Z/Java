ab8135d437d2acac5cd542bda5328dcfe41f6443
hriza2011 <161128718+hriza2011@users.noreply.github.com>
2025-06-28 18:06:36 +0530

    Arithmetic Operators

---
+++ ArithmeticOperators.java
@@ -0,0 +1,40 @@
+public class ArithmeticOperators {
+    public static void main (String[] args) {
+        
+        //variables
+        int num1, num2, result;
+        num1 = 20;
+        num2 = 5;
+        res = 0;
+
+        //Addition
+        res = num1 + num2;
+        System.out.println("20 + 5 = " + res) // '+' is used for addition and concatenation
+        //reseting the result to zero for next operation
+        res = 0;
+
+        //Subtraction
+        res = num1 - num2;
+        System.out.println("20 - 5 = " + res) // '+' is used for addition and concatenation
+        //reseting the result to zero for next operation
+        res = 0;
+
+        //Multiplication
+        res = num1 * num2;
+        System.out.println("20 * 5 = " + res) // '+' is used for addition and concatenation
+        //reseting the result to zero for next operation
+        res = 0;
+
+        //Division
+        res = num1 / num2;
+        System.out.println("20 / 5 = " + res) // '+' is used for addition and concatenation
+        //reseting the result to zero for next operation
+        res = 0;
+
+        //Modulus
+        res = num1 % num2;
+        System.out.println("20 % 5 = " + res) // '+' is used for addition and concatenation
+        //reseting the result to zero for next operation
+        res = 0;
+    }
+}
