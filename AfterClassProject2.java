af27d3998253f33fbddb150e8ba8fdcf240bf5e4
hriza2011 <161128718+hriza2011@users.noreply.github.com>
2025-06-28 18:28:58 +0530

    After Class Project 2

---
+++ AfterClassProject2.java
@@ -0,0 +1,15 @@
+public class AfterClassProject2
+    {
+        public static void main(String [] args)
+        {
+            // Variables
+            int febDays = 29;
+            String result;
+
+            //Checking
+            result = (febDays == 29) ? " Leap Year " : " Not a Leap Year " ;
+
+            //Output
+            System.out.println("The year is" + result + "as it has " + febDays + " days.");
+        }
+    }
